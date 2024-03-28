f43e3b5d63be976fca384795c911b2083ec9be89
Raffaelbdl <raffaelbdl@gmail.com>
2024-03-28 10:59:59 +0900

    Add DEBUG

--- shaberax/constants.py
+++ shaberax/constants.py
@@ -6,5 +6,6 @@ def color(text: str, ansi: AnsiCodes) -> str:
     return ansi + text + Style.RESET_ALL
 
 
+DEBUG = color("%(levelname)s", Back.YELLOW)
 ERROR = color("%(levelname)s", Back.RED)
 TELEGRAM = color("TELEGRAM", Fore.BLUE)
