09d1f1f3d8db03f60ebd1915d660ffc59845e206
Raffaelbdl <raffaelbdl@gmail.com>
2024-03-28 15:01:12 +0900

    Prevent propagation of handlers

--- shaberax/logger.py
+++ shaberax/logger.py
@@ -6,11 +6,13 @@ from shaberax.constants import DEBUG, ERROR
 def create_stream_logger(name: str, formatter: logging.Formatter) -> logging.Logger:
     """Creates a Logger given a name and a formatter."""
     logger = logging.getLogger(name)
+    logger.propagate = False
 
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
-    logger.addHandler(handler)
     logger.setLevel(logging.INFO)
 
     return logger
